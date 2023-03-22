05450f006d4bb1b8d2b26708194e1a33b4e4e4dc
Paul Kurdiumov <prototype97971@gmail.com>
2023-03-22 16:58:00 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+ let incr = 10,
+     decr = 10;
+
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr);
+console.log(decr);
+
+
+console.log(7%2);
+
+
+console.log((2+2)*2 != 8);
+
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
