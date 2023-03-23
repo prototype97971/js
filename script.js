0e08754497e2240e85257bd70130799084ab6fb8
Paul Kurdiumov <prototype97971@gmail.com>
2023-03-23 13:25:41 +0200

    Lesson 21 if statement more deyails

--- script.js
+++ script.js
@@ -1,40 +1,70 @@
 "use strict"
 
- if (4==9) {
-    console.log(object);
- } else {
-    console.log("err");
- }
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//    console.log("Наївся");
+// }
 
 
- 
+// console.log((hamburger && fries));
 
-//  if (num<49) {
-//    console.log("err");
-//  } else if (num>100) {
-//    console.log("so big");
-//  } else {
-//    console.log("ok");
-//  }
 
- 
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && "djbsjbd");
 
-//  (num === 50) ? console.log("ok") : console.log("err");
 
- const num = 50;
 
- switch (num) {
-   case 49:
-      console.log("nooooo");
-      break;
-   case 100:
-      console.log("nooooo");
-      break;
-   case 50:
-      console.log("Eba popav");
-      break;
-   default:
-      console.log("Lol");
-      break;
 
- }
+
+// const hamburger = 3;
+// const fries = 11;
+// const cola = 10;
+
+// console.log((hamburger === 3 && cola && fries));
+
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//    console.log("Нажерлись");
+// } else {
+//    console.log("Валим");
+// }
+
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//    console.log("ну ок");
+// } else {
+//    console.log("Валим");
+// }
+
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+// let johnReport, alexReport, samReport, mariaReport = "done";
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+} else {
+   console.log("LOX");
+}
