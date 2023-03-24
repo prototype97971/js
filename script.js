3b4f3cf9ac8d6cf63e2a05dbe45643223fa8f030
Paul Kurdiumov <prototype97971@gmail.com>
2023-03-24 21:13:11 +0200

    Coding exercise 6 lesson 24-27

--- script.js
+++ script.js
@@ -1,194 +1,199 @@
 "use strict"
 
+// Homework 6
 
-// for(let i=0; i<3; i++) {
-//    console.log(i);
-//    for(let j=0; j<3; j++) {
-//       console.log(j);
-      
-//    }
+// // #1
+
+// function sayHello (name) {
+//    return `Привіт ${name}`
 // }
 
-
-// let result = "";
-// const length = 7;
-
-
-// for (let i = 1; i < length; i++) {
-
-//    for (let j = 0; j < i; j++) {
-//       result += "*";
-
-//    }
+// sayHello('Kik')
+// console.log(sayHello('Kik'));
 
 
-
-//    result += "\n";
-// }
-// console.log(result);
-
-
-// first :for(let i=0; i<3; i++) {
-//    console.log(`First level: ${i}`);
-//    for(let j=0; j<3; j++) {
-//       console.log(`Second level: ${j}`);
-//       for(let k=0; k<3; k++) {
-//          if ( k===2 ) break first;
-//          console.log(`Third level: ${k}`);
-
-         
-//       }
-      
-//    }
-// }
-
-// Homework
-
-// Я 1
-
-// const length = 11;
-
-// for (let i=0; i<length; i++) {
-//    if (i>4 && i<length) {
-//       console.log(i);
-//    }
-// }
-
-// // Бодя 1
-
-// let n = 5;
-//     while (n < 11) {
-//         console.log(n);
-//         n++;
-//     }
-    
-
-
-
-//    //  Я 2
-
-
-
-//    for (let i=20; i > 10; i--) {
-//       if (i==13) {
-//          break;
-//       }
-//       console.log(i);
-//    }
-
-
-// // Бодя
-
-// for (let i = 20; i > 10; i--) {
-//    if (i == 13) {
-//          break;
-//    }
-//    console.log(i);
-// }
-
-
-// // Я
-
-// const limit = 11;
-// for (i=0; i<limit; i++ ) {
-//    if (i % 2 < 1) {
-//       console.log(i);
-//    }
-// }
-
-// // Бодя
-// for (let i = 2; i <= 10; i = i + 2) {
-//    console.log(i);
-// }
-
-// // Я
-// let i = 2;
-
-// while (i <= 16) {
-//    i++;
-//    if (i % 2 === 0){
-//       continue
-//    }
-//    console.log(i); 
-// }
-
-
-// for (let i = 2; i <= 16; i++) {
-//    if (i % 2 === 0) {
-//        continue;
-//    } else {
-//        console.log(i);
-//    }
-// }
-
-// My
-
-// let arr = [];
-// let arrIndex = 0;
-// for (let i=5; i<11; i++) {
-//       arr[arrIndex] = i;
-//       arrIndex++;
-//    }
-// console.log(arr);
-
-
-// Homework with *
-
-// const arr = [1, 2, 7, 8, 9, 11, 3, 12,];
-
-// const result = [];
-
-// for (let i = 0; i <arr.length; i++) {
-//    result[i]=arr[i];
-   
-
-// };
-// console.log(result);
 
 
 // // #2
 
-// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// function arrNumbers(digit) {
+//    return [digit - 1, digit, digit + 1]
 
-// let less = 4;
+// }
+// console.log(arrNumbers(10));
 
-// for (let i = 0; i<data.length; i++ ) {
-//    if (typeof(data[i]) === 'string') {
-//       data[i]= `${data[i]} - done`;
-//    } else {
-//       data[i] = data[i] * 2;
-//    }
-// };
-// console.log(data);
 
 // // #3
 
-// const data = [5, 10, 'Shopping', 20, 'Homework'];
-// const result = [];
+// function getMathResult (numF, numS) {
+//    if (typeof(numS) !== 'number' || numS <= 0 ) {
+//       console.log(numF);
+//       return numF;
+//    }   
 
-// for (let i = 1; i<=data.length; i++) {
+//    let str = "";
 
-//    result[i-1] = data[data.length-i];
+//    for (i=1; i <=numS; i++) {
+//       if(i === numS) {
+//          str += `${numF * i}`;
 
+//       } else {
+//          str += `${numF * i}---`;
+//       }
+//    }
 
-// }
-// console.log(result);
-
-
-// Pyramid not me
-
-
-// const lines = 6;
-// let result = '';
-
-// for (let i = 0; i <= lines; i++) {
-//     for (let j = 0; j < lines - i; j++) {
-//         result += " ";
-//     }
-//     for (let j = 0; j < 2 * i + 1; j++) {
-//         result += "*";
-//     }
-//     result += "\n";
+//    return str
 // }
 
-// console.log(result)
+// console.log(getMathResult(10, 5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // lesson 26
+
+// const usdCurrs = 28;
+// const discount = 0.9;
+
+
+
+
+
+// function convert (amount, currs) {
+//    return currs * amount;
+// }
+
+// function promotion(result) {
+//    console.log(result * discount);  
+// }
+
+// const res = convert(500, usdCurrs);
+
+
+// promotion(res);
+
+// function test () {
+//    for (let i = 0; i < 5; i++ ) {
+//       console.log(i);
+//       if(i === 3) return
+
+//    };
+//    console.log("done");
+// }
+// test();
+
+
+// function doNothing (){};
+// console.log(doNothing() === undefined);
+
+
+
+
+
+
+
+
+
+
+// // lesson 25
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//    console.log(text);
+//    num = 10;
+
+// }
+
+// showFirstMessage("Hi man");
+// console.log(num);
+
+// // function calc(a, b) {
+// //    return (a+b);
+// // }
+
+// // console.log(calc(4 ,3));
+// // console.log(calc(5, 5));
+
+
+// function ret() {
+//    let num = 50;
+//    return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// const logger = function() {
+//    console.log("woo woo woo");
+// };
+
+// logger();
+
+// const calc = (a, b) => { return a + b};
+
+
+
+
+
+
+
+
+// // Lesson 24
+// const numberOfFilms = prompt("Скільки фільмів ти переглянув?", "");
+
+// const personalMoviDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+
+// };
+
+// for (let i = 0; i < 2; i++) {
+//    const a = prompt("Один з останніх фільмів який ти переглянув?", ""),
+//          b = prompt("Оціни цей фільм від 0.1 до 10?", "");
+
+//    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//       personalMoviDB.movies[a]=b;
+//       console.log("done");
+
+//    } else {
+//       console.log("user Lopux");
+//       i--;
+//    }
+   
+
+// };
+
+// if (personalMoviDB.count < 10) {
+//    console.log("Omg мало");
+// } else if (personalMoviDB.count >=10 && personalMoviDB.count < 30) {
+//    console.log("oMG THIS NORMAS");
+// } else if (personalMoviDB.count >=30) {
+//    console.log("Omg Monster");
+// } else {
+//    console.log("WTF MAN");
+// }
+
+
+
+// console.log(personalMoviDB);
